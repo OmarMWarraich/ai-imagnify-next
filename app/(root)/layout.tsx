96523bc0516fd1c27a11e1852e5a76f5350478e0
OmarMWarraich <earthling986@gmail.com>
2024-02-16 20:48:02 +0500

    feat: Routing & Folder Structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const Layout = (
+    { children } : Props
+    ) => {
+  return (
+    <main 
+        className="root"
+    >
+        <div className="root-container">
+            <div className="wrapper">
+                {children}
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Layout
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TransformationsPage = (props: Props) => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
